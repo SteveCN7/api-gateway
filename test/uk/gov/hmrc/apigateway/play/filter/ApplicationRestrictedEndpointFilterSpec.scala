d24615c2f4e0f5449d0fcfd6deb980e24950a101
Thomas VANDEVELDE <thomas.vandevelde@digital.hmrc.gov.uk>
2016-12-29 16:31:18 +0000

    API-2216 - Clean up

--- test/uk/gov/hmrc/apigateway/play/filter/ApplicationRestrictedEndpointFilterSpec.scala
+++ test/uk/gov/hmrc/apigateway/play/filter/ApplicationRestrictedEndpointFilterSpec.scala
@@ -26,6 +26,7 @@ import uk.gov.hmrc.apigateway.exception.GatewayError
 import uk.gov.hmrc.apigateway.exception.GatewayError.{InvalidCredentials, MissingCredentials}
 import uk.gov.hmrc.apigateway.model.AuthType.APPLICATION
 import uk.gov.hmrc.apigateway.model._
+import uk.gov.hmrc.apigateway.service.AuthorityService
 import uk.gov.hmrc.apigateway.util.HttpHeaders._
 import uk.gov.hmrc.play.test.UnitSpec
 
